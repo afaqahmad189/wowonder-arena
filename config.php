5ebe088bd27789ff97883b7f87d296cf586ced8b
afaqahmad189 <afaqzafar789@gmail.com>
2020-12-11 11:18:31 +0500

    original version

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.wowonder.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com   
+// +------------------------------------------------------------------------+
+// | WoWonder - The Ultimate PHP Social Networking Platform
+// | Copyright (c) 2017 WoWonder. All rights reserved.
+// +------------------------------------------------------------------------+
+// @header('Location: install');
+// exit();
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "root";
+// MySQL Database Password
+$sql_db_pass = "";
+// MySQL Database Name
+$sql_db_name = "db_wowonder";
+
+// Site URL
+$site_url = "http://localhost/wowonder/"; // e.g (http://example.com)
+?>
